abba9d8ea3693048032798639dd8a64f524b73f7
thiago.leite <thiago.leite@everest-erp.com>
2023-11-14 16:15:34 -0300

    chore: add more logs

--- main.go
+++ main.go
@@ -16,16 +16,23 @@ func main() {
 		Artifacts: strings.Split(strings.TrimSpace(os.Getenv("ARTIFACTS")), "\n"),
 	}
 
+	log.Printf("starting the caching process with:")
+	log.Printf("Key=%s\n", action.Key)
+	log.Printf("Artifacts=%v\n", action.Artifacts)
+	log.Printf("Bucket=%s\n", action.Bucket)
+
 	switch act := action.Action; act {
 	case PutAction:
 		if len(action.Artifacts[0]) <= 0 {
 			log.Fatal("No artifacts patterns provided")
 		}
 
+		log.Printf("starting the tar process")
 		if err := Tar(action.Key, action.Artifacts); err != nil {
 			log.Fatal(err)
 		}
 
+		log.Printf("uploading file to s3")
 		if err := PutObject(action.Key, action.Bucket, action.S3Class); err != nil {
 			log.Fatal(err)
 		}
@@ -37,10 +44,12 @@ func main() {
 
 		// Get and and unzip if object exists
 		if exists {
+			log.Printf("reading from s3")
 			if err := GetObject(action.Key, action.Bucket); err != nil {
 				log.Fatal(err)
 			}
 
+			log.Printf("starting the untar process")
 			if err := Untar(action.Key); err != nil {
 				log.Fatal(err)
 			}
@@ -48,10 +57,12 @@ func main() {
 			log.Printf("No caches found for the following key: %s", action.Key)
 		}
 	case DeleteAction:
+		log.Printf("deleting from s3")
 		if err := DeleteObject(action.Key, action.Bucket); err != nil {
 			log.Fatal(err)
 		}
 	default:
 		log.Fatalf("Action \"%s\" is not allowed. Valid options are: [%s, %s, %s]", act, PutAction, DeleteAction, GetAction)
 	}
+	log.Printf("caching process finished!")
 }
