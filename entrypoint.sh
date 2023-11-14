032917cb469d3fc9895b44d80dcbcf9d33808045
thiago.leite <thiago.leite@everest-erp.com>
2023-11-14 14:34:42 -0300

    chore: fix binary names

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Select right go binary for runner os
-$GITHUB_ACTION_PATH/dist/$(echo "$OS_$ARCH" | tr "[:upper:]" "[:lower:]")
+$GITHUB_ACTION_PATH/dist/$(echo "$OS-$ARCH" | tr "[:upper:]" "[:lower:]")
